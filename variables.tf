0a4d881f6fbb5c1bb5a577cd57fb8651500ad967
TK1359 <toomaj1980@gmail.com>
2025-03-26 14:34:43 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "region" {
 description = "name of region"
+default = "eu-west-2"
 }
-
 
 variable "ami" {
-default = "ami-123"
+  description = "AMI to use for the instance. Required unless launch_template is specified and the Launch Template specifes an AMI."
+  type        = string
+  default     = null
 }
-
 
 variable "instance_type" {
 default= "t2.micro"
