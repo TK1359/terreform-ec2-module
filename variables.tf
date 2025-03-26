e27ef5d8e7c11983def7dab97769df262f6855fc
TK1359 <toomaj1980@gmail.com>
2025-03-26 14:27:36 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,13 @@
-variable "region" {}
+variable "region" {
+default= "eu-west-2"
+}
 
 
-variable "ami" {}
+variable "ami" {
+default = "ami-091f18e98bc129c4e"
+}
 
 
-variable "instance_type" {}
+variable "instance_type" {
+default= "t2.micro"
+}
